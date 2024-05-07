b7d039f16525dafe8c8a7d71a1d5964bb88a34dd
nam <namnam290703@gmail.com>
2024-05-07 12:47:25 +0700

    test

---
+++ Myshop/Areas/customs/customsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MyShop.Areas.customs
+{
+    public class customsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "customs";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "customs_default",
+                "customs/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Myshop/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using MyShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        // khai báo db
+        shoeEntities s = new shoeEntities();
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(string uname,string psw)
+        {
+            // check điều kiện
+            int countz = s.users.Count(m => m.username.ToLower() == uname.ToLower() && m.password.ToLower() == psw.ToLower());
+            if(countz ==1 )
+            {
+                //tạo session giá trị 
+                Session["user"] = uname;
+                ViewBag.user = uname;
+            }    
+            else
+            {
+                TempData["error"] = "Please check your user or password again ";
+                 return View();
+            }
+            return RedirectToAction("home","Home");
+        }
+    }
+}
